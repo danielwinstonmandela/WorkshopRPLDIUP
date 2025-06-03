bf353180a42e11140e75b7827c16e9c5c005c3b4
RinAdriann <149231126+RinAdriann@users.noreply.github.com>
2025-06-04 01:59:20 +0700

    Update page.jsx

--- QuickCart/src/app/seller/page.jsx
+++ QuickCart/src/app/seller/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import { assets } from "@/src/assets/assets";
+import { assets } from "@/assets/assets";
 import Image from "next/image";
 
 const AddProduct = () => {
