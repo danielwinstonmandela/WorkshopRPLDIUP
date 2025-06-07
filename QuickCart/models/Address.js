bada7e8d8b401b5fbf98ebb83da3f4290384d5a0
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-07 21:53:14 +0000

    revisiii

--- QuickCart/models/Address.js
+++ QuickCart/models/Address.js
@@ -4,7 +4,7 @@ const addressSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     fullname: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    pincode: { type: Number, required: false },
+    pincode: { type: Number, required: true },
     area: { type: String, required: true },
     city: { type: String, required: true },
     state: { type: String, required: true },
