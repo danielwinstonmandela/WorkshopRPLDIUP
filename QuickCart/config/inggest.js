08af51635cbd0f2738bf22765a873e8a01635c37
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-07 12:04:27 +0000

    typo

--- QuickCart/config/inggest.js
+++ QuickCart/config/inggest.js
@@ -48,7 +48,7 @@ export const syncUserDeletion = inngest.createFunction(
     {
         id: 'delete-user-with-clerk'
     },
-    { evernt: 'clerk/user.deleted' },
+    { event: 'clerk/user.deleted' },
     async ({event}) => {
         const {id } = event.data
 
