dcadc530ddd256ba9b050e6b6b6cdf56eb30eaf7
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-07 14:26:07 +0000

    ingget sign and event key baru

--- QuickCart/components/Footer.jsx
+++ QuickCart/components/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © GreatStack.dev All Right Reserved.
+        WRPL FINAL PROJECT © CSB IUP
       </p>
     </footer>
   );
