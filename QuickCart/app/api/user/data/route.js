9e9c2afebc3f19fa87f362af8db94d4f150c79c5
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-07 19:01:16 +0000

    notdefined

--- QuickCart/app/api/user/data/route.js
+++ QuickCart/app/api/user/data/route.js
@@ -1,6 +1,7 @@
 import { getAuth } from "@clerk/nextjs/server";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
 
 
 export async function GET(request) {
