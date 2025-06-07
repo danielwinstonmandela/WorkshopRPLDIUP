5209487cbac3932bbcb3899bcf69cd980fc92e8e
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-07 21:39:00 +0000

    addresses

---
+++ QuickCart/app/api/user/add-address/route.js
@@ -0,0 +1,19 @@
+import { getAuth } from '@clerk/nextjs/server';
+import connectDB from '@/config/db';
+import Address from '@/models/address';
+
+export async function POST(request) {
+    try {
+        
+        const { userId } = getAuth(request)
+        const { address } = await request.json()
+
+        await connectDB()
+        const newAddress = await Address.create({...address, user: userId })
+
+        return NextResponse.json({ success: true, messqage: "Address added successfully", newAddress }) 
+
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
