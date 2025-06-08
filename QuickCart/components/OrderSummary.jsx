2d3ed590668129d76d7646ab3e4e2ff583b1fdb3
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-08 00:06:05 +0000

    yesyesyss

--- QuickCart/components/OrderSummary.jsx
+++ QuickCart/components/OrderSummary.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import cart from "@/cart";
 
 const OrderSummary = () => {
 
@@ -37,7 +38,39 @@ const OrderSummary = () => {
   };
 
   const createOrder = async () => {
+    try {
+      
+      if (!selectedAddress) {
+        return toast.error('Please select an address');
+      }
 
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({product:key, quantity: cartItems[key]}))
+      cartItemsArray = cartItemsArray.filter(item => item.quantity > 0)
+
+      if (cartItemsArray.length === 0) {
+        return toast.error(' cart is empty')
+      }
+
+      const token = await getToken()
+
+      const { data } = await axios.post('/api/order/create', {
+        address: selectedAddress._id,
+        items: cartItemsArray
+      }, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+
+      if (data.success) {
+        toast.success(data.message)
+        setCartItems({})
+        router.push('/orders-placed')
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
