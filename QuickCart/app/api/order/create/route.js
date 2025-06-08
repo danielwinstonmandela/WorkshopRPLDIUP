23dd645f75eccb82094c90e439de3d0db67a81c4
NinoW <64418184+Nino-123@users.noreply.github.com>
2025-06-08 00:08:29 +0000

    ye

--- QuickCart/app/api/order/create/route.js
+++ QuickCart/app/api/order/create/route.js
@@ -1,4 +1,4 @@
-import { getAuth } from "@clerk/nextjs/server";
+import { getAuth, user } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Product from "@/models/Product"; // Assuming you have a Product model to fetch product details
 import { inngest } from "@/config/inngest";
